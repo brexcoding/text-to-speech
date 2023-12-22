ead94a759352cc189e617f87cef5e71a06bb9a8a
abd elhamid <houariabdelhamid5@gmail.com>
2023-12-21 18:08:40 -0800

    added cover image and logo

--- testing.py
+++ testing.py
@@ -15,17 +15,23 @@ MDBoxLayout:
     orientation: "vertical"
 
     MDTopAppBar:
-        title: "Select the pdf file that you want to convert it to mp3 "
-        left_action_items: [["menu", lambda x: None]]
+        title: "brexcoding pdf to mp3 converter"
+        left_action_items: [[ "logo.png", lambda x: None]]
         elevation: 3
 
     MDFloatLayout:
-        
+
         MDRoundFlatIconButton:
-            text : "select pdf to it to mp3"
+            text: "select the pdf "
             icon: "folder"
-            pos_hint: {"center_x": .5, "center_y": .5}
-            on_release : app.select_path()
+            pos_hint: {"center_x": .5, "center_y": .3}
+            on_release: app.file_manager_open()
+        FitImage:
+            source: "photo.png"
+            size_hint_y: .60
+            pos_hint: {"top": 1}
+            radius: 36, 36, 0, 0
+    
 '''
 
 
@@ -34,8 +40,6 @@ class brexcoding_tts(MDApp):
         super().__init__(**kwargs)
         Window.bind(on_keyboard=self.events)
         self.manager_open = False
-
-        self.chosen_file_path = self.select_path
         self.file_manager = MDFileManager(
             exit_manager=self.exit_manager, select_path=self.select_path
         )
@@ -49,36 +53,17 @@ class brexcoding_tts(MDApp):
         self.file_manager.show(os.path.expanduser("~"))  # output manager to the screen
         self.manager_open = True
 
-    def exit_manager(self, *args):
-        '''Called when the user reaches the root of the directory tree.'''
-
-        self.manager_open = False
-        self.file_manager.close()
-
-    def events(self, instance, keyboard, keycode, text, modifiers):
-        '''Called when buttons are pressed on the mobile device.'''
-
-        if keyboard in (1001, 27):
-            if self.manager_open:
-                self.file_manager.back()
-        return True
-
-    def select_path(self, path):
-        # ... other code
-        self.file_manager.show(os.path.expanduser("~"))
-        self.manager_open = True
-        path = os.path.expanduser("~")
-        file_manager = MDFileManager(select_path=self.select_path)
-        file_manager.show(path)
-        self.chosen_file_path = path 
-        self.extract_and_save_pdf_audio()  # Call the conversion function
-        return self.chosen_file_path # Return the selected path
-
-        
-    def extract_and_save_pdf_audio(self,*args):
-
-        if self.select_path.endswith(".pdf"):  # Correctly check for .pdf extension
-            pdf_path = self.chosen_file_path
+    def select_path(self, path: str):
+        '''
+        It will be called when you click on the file name
+        or the catalog selection button.
+        :param path: path to the selected directory or file;
+        '''
+        self.exit_manager()
+        toast(path)
+    
+        if path.endswith(".pdf"): 
+            pdf_path = path
             reader = PdfReader(pdf_path)
             engine = pyttsx3.init()
             rate = engine.getProperty('rate')
@@ -101,30 +86,44 @@ class brexcoding_tts(MDApp):
                     progress.append(percent)
                     toast(f"Extracting text... {percent:.2f}% complete")
 
-                # Save the extracted text to an MP3 file
-                engine.save_to_file(''.join(all_text), 'pdf.mp3')
+                # creating a BREXCoding folder  to contain the mp3 files
+                desktop_path = os.path.expanduser("~/Desktop")
+                folder_name = "BREXCoding"  # Customize the folder name as needed
+                folder_path = os.path.join(desktop_path, folder_name)
+                os.makedirs(folder_path, exist_ok=True)  # Create the folder if it doesn't exist
+                
+                pdf_name_with_extension = os.path.basename(pdf_path)
+                # removing the .pdf extension from the string
+                pdf_name = pdf_name_with_extension[:-4]
+                full_path = os.path.join(folder_path, f"{pdf_name}.mp3")
+
+                engine.save_to_file(''.join(all_text), full_path)
                 engine.runAndWait()
 
             except (FileNotFoundError, PyPDF2.errors.PdfReadError) as e:
                 raise RuntimeError(f"Error processing PDF: {e}")
             except Exception as e:
                 raise RuntimeError(f"An error occurred during text-to-speech: {e}")
+                
         else :
-            print('no')
+            toast('you should select a pdf sir .!')
 
-    
+ 
 
+
+    def exit_manager(self, *args):
+        '''Called when the user reaches the root of the directory tree.'''
+
+        self.manager_open = False
+        self.file_manager.close()
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        '''Called when buttons are pressed on the mobile device.'''
+
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
 
 
 brexcoding_tts().run()
-
-
-
-path = os.path.expanduser("~")  # path to the directory that will be opened in the file manager
-file_manager = MDFileManager(
-    exit_manager=self.exit_manager,  # function called when the user reaches directory tree root
-    select_path=self.select_path,  # function called when selecting a file/directory
-)
-file_manager.show(path)
-print(path)
-
